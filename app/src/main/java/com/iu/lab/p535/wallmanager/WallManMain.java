b1db4ed29803b559d3b1144b774e9795d77d1fc4
RashmiRB <rbidanta@yahoo.in>
2017-02-08 16:14:43 -0500

    Initial commit: Project Created

---
+++ app/src/main/java/com/iu/lab/p535/wallmanager/WallManMain.java
@@ -0,0 +1,13 @@
+package com.iu.lab.p535.wallmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WallManMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wall_man_main);
+    }
+}
