bb244dd08d39a2c223f691db37aae97843af1c14
jaidevyd <jaidevyd@gmail.com>
2017-02-09 13:32:49 -0500

    test

--- app/src/main/java/com/iu/lab/p535/wallmanager/WallManMain.java
+++ app/src/main/java/com/iu/lab/p535/wallmanager/WallManMain.java
@@ -9,5 +9,6 @@ public class WallManMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wall_man_main);
+        System.out.print("Lab");
     }
 }
